fcb97a08b320de82f875300a1f5423196c9b0569
邢浩东 <5487156+kuaishou1@user.noreply.gitee.com>
2023-07-26 22:42:49 +0800

    feat: 学习react

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Hello from './components/Hello';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Hello message={'你好邢浩东'}></Hello>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Hello.tsx
@@ -0,0 +1,5 @@
+const Hello = (props: any) => {
+
+    return <h2>{props.message}</h2>
+}
+export default Hello
